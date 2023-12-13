ae92f14f728241c31337a1cde08cdb537c9e630a
mrxie <844497663@qq.com>
2023-12-13 23:50:29 +0800

    12-13 commit

---
+++ MyCommon/src/main/java/org/example/enums/Result.java
@@ -0,0 +1,19 @@
+package org.example.enums;
+
+/**
+ * @Description:org.example.enums
+ * @Date:2023/12/13
+ * @Author:谢锦创
+ */
+public enum Result {
+    SUCCESS("0000", "成功"),
+    FAIL("0001", "失败");
+
+    private String code;
+    private String msg;
+
+    Result(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+}
---
+++ MyFeign/src/main/java/org/example/controller/RedisController.java
@@ -0,0 +1,33 @@
+package org.example.controller;
+
+import org.example.enums.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @className: RedisController
+ * @description:
+ * @author: sh.Liu
+ * @date: 2022-03-08 14:28
+ */
+@RestController
+@RequestMapping("redis")
+public class RedisController {
+
+    @Autowired
+    private final RedisTemplate redisTemplate;
+
+    public RedisController(RedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @GetMapping("save")
+    public Result save(String key, String value){
+        redisTemplate.opsForValue().set(key, value);
+        return Result.SUCCESS;
+    }
+
+}
